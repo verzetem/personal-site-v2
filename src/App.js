de9a5ddc4915999ff2f6517a24712f5437a3ef8d
chris <verzetem@gmail.com>
2018-11-14 13:32:00 -0700

    imported bootstrap/reactstrap, minor changes to App.css, changed title in index.html, removed favicon.ico

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Alert, Badge } from 'reactstrap';
 
 class App extends Component {
+
   render() {
+
     return (
+
       <div className="App">
         <h1>Hello World</h1>
+        
       </div>
+
     );
   }
 }
