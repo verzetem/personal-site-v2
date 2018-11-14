b0ddbed8989844a33ffef3587d0811d206397873
chris <verzetem@gmail.com>
2018-11-14 13:43:01 -0700

    added components folder, added Header.jsx, imported/added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Alert, Badge } from 'reactstrap';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
 
+// logger = () => {
+//   console.log('sefa')
+// }
+
   render() {
 
-    return (
+  const Index = () => <h2>Home</h2>;
+  const About = () => <h2>About</h2>;
+  const Users = () => <h2>Users</h2>;
 
-      <div className="App">
-        <h1>Hello World</h1>
-        
-      </div>
+    return (
+      
+      <Router>
+        <div className="App">
+          <nav>
+
+
+                <Link to="/">Home</Link>
+
+
+                <Link to="/about/">About</Link>
+
+
+                <Link to="/users/">Users</Link>
+
+
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/users/" component={Users} />
+
+        </div>
+      </Router>
 
     );
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+	
+	return (
+		<h3>Header.jsx</h3>
+
+
+
+	)
+}
+
+export default Header;
